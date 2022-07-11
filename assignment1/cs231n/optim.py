e30e81d916d247ba68b6e9ff1332c7d916e5c982
aoli-gei <937434992@qq.com>
2022-07-11 16:19:05 +0800

    完成两层网络

--- assignment1/cs231n/optim.py
+++ assignment1/cs231n/optim.py
@@ -5,6 +5,8 @@ This file implements various first-order update rules that are commonly used
 for training neural networks. Each update rule accepts current weights and the
 gradient of the loss with respect to those weights and produces the next set of
 weights. Each update rule has the same interface:
+该文件实现了用于训练神经网络的各种常用一阶更新规则。每个更新规则都接受当前权重和
+损失相对于这些权重的梯度并产生下一组权重。每个更新规则都有相同的接口:
 
 def update(w, dw, config=None):
 
@@ -24,15 +26,21 @@ Returns:
 NOTE: For most update rules, the default learning rate will probably not
 perform well; however the default values of the other hyperparameters should
 work well for a variety of different problems.
+对于大多数更新规则，默认学习率可能不会
+表现良好;但是其他超参数的默认值应该
+适用于各种不同的问题。
 
 For efficiency, update rules may perform in-place updates, mutating w and
 setting next_w equal to w.
+为了提高效率，更新规则可以执行就地更新，改变 w 和
+设置 next_w 等于 w。
 """
 
 
 def sgd(w, dw, config=None):
     """
     Performs vanilla stochastic gradient descent.
+    执行普通随机梯度下降
 
     config format:
     - learning_rate: Scalar learning rate.
@@ -45,7 +53,7 @@ def sgd(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    w-=dw*config['learning_rate']
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
